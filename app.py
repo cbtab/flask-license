4d9b89254eb0ea96cdfa751972477cff837a4127
chalermchat <chalermchat.tab@gmail.com>
2024-02-05 13:28:39 +0700

    change app.py

--- app.py
+++ app.py
@@ -1,68 +1,74 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
-from ultralytics import YOLO
 from PIL import Image, ImageFilter
 import easyocr
 import io
 import base64
-import os
+from ultralytics import YOLO
 
+model = YOLO('best.pt')
 app = Flask(__name__)
 CORS(app)
-model = YOLO('best.pt')
+
+def save_base64_image(base64_string, file_extension='jpeg'):
+    image_bytes = base64.b64decode(base64_string)
+    image = Image.open(io.BytesIO(image_bytes))
+    temp_file_path = f'temp_image.{file_extension}'
+    image.save(temp_file_path)
+    return temp_file_path
+
+def crop_and_base64_objects(image, results):
+    base64_images = []
+    if results:
+        for i, result in enumerate(results):
+            box = result.boxes[0].xyxy[0].tolist()
+            xmin, ymin, xmax, ymax = map(int, box)
+            cropped_image = image.crop((xmin, ymin, xmax, ymax))
+
+            buffered = io.BytesIO()
+            cropped_image.save(buffered, format="jpeg")
+            base64_string = base64.b64encode(buffered.getvalue()).decode('utf-8')
+            base64_images.append(base64_string)
+
+    return base64_images
 
 @app.route('/', methods=['GET'])
 def hello_world():
-    return "server is running"
+    return "Server is running"
 
 @app.route('/process_image', methods=['POST'])
 def process_image():
     try:
+        if 'image' not in request.json:
+            return jsonify({"error": "No image provided"}), 400
 
         base64_image = request.json['image']
-
         image_bytes = base64.b64decode(base64_image)
+        image = Image.open(io.BytesIO(image_bytes))
+        results = model(image)
+
+        base64_images = crop_and_base64_objects(image, results)
+
+        for base64_image in base64_images:
+            image_bytes2 = base64.b64decode(base64_image)
+            image2 = Image.open(io.BytesIO(image_bytes2))
+            
+
+            
+        print(base64_images)
+        image_format = image.format.lower() if image.format else None
+        allowed_characters = set('0123456789กขฃคฅฆงจฉชซฌญฎฏฐฑฒณดตถทธนบปผฝพฟภมยรลวศษสหฬอฮ๐๑๒๓๔๕๖๗๘๙')
 
 
-        image_path = 'received_image.png'
-        with open(image_path, 'wb') as image_file:
-            image_file.write(image_bytes)
+        reader = easyocr.Reader(['th'])
+        ocr_result = reader.readtext(image)
 
-
-        results = model(image_path)
-        boxes = results[0].boxes
-
-        if len(boxes) > 0:
-            box = boxes[0]
-            xmin, ymin, xmax, ymax = box.xyxy[0].tolist()
-
-            image = Image.open(image_path)
-            cropped_image = image.crop((xmin, ymin, xmax, ymax))
-            sharpened_image = cropped_image.filter(ImageFilter.SHARPEN)
-
-    
-            sharpened_image_path = 'sharpened_image.png'
-            sharpened_image.save(sharpened_image_path)
-
-            reader = easyocr.Reader(['th'])
-            ocr_result = reader.readtext(sharpened_image_path)
-        else:
-            image = Image.open(image_path)
-
-            reader = easyocr.Reader(['th'])
-            ocr_result = reader.readtext(image_path)
-
-        recognized_texts = [text_info[1] for text_info in ocr_result]
-        print(recognized_texts)
-        return jsonify({"recognized_texts": recognized_texts})
+        recognized_texts = ["".join(char for char in text_info[1] if char in allowed_characters) for text_info in ocr_result]
+        print('Recognized texts:', recognized_texts)
+        return jsonify({"recognized_texts": recognized_texts, "image_format": image_format, "base64_images": base64_images})
 
     except Exception as e:
         return jsonify({"error": str(e)}), 500
-    finally:
-        if os.path.exists(image_path):
-            os.remove(image_path)
-        if os.path.exists(sharpened_image_path):
-            os.remove(sharpened_image_path)
 
-# if __name__ == '__main__':
-#     app.run()
+if __name__ == '__main__':
+    app.run()
